d491cecce0477f91dbe260594abdea0a2f5c5edb
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-10 18:15:45 +0700

    feat : add route,controller and model product

---
+++ src/routes/product.js
@@ -0,0 +1,21 @@
+const products = require('express').Router();
+const { validateProduct } = require('../helpers/validation')
+    // const { verifyUser } = require('../helpers/auth');
+
+const {
+    getProducts,
+    getProduct,
+    insertProduct,
+    updateProduct,
+    updatePatchProduct,
+    deleteProduct
+} = require('../controllers/product');
+
+products.get('/', getProducts)
+products.get('/:id', getProduct)
+products.post('/', insertProduct);
+products.put('/:id', updateProduct);
+products.patch('/:id', updatePatchProduct);
+products.delete('/:id', deleteProduct);
+
+module.exports = products;
