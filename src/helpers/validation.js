3f4fa3737e8d349c5f47f1670a703b03f77599aa
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-11 19:25:31 +0700

    feat : add validation for user

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -3,122 +3,164 @@ const deliveryMethodModel = require('../models/deliveryMethod')
 const userModel = require('../models/user')
 const sizeModel = require('../models/size')
 
-exports.validationDataProducts = async (data) => {
-  let result = null
-  if (data.name == null || data.name == '') {
-    result = { name: 'Name must be filled' }
-  }
-
-  if (data.category_id == null || data.category_id == '') {
-    result = { ...result, category_id: 'Id category must be filled.' }
-  } else {
-    const getDataCategory = await categoryModel.getDataCategory(data.category_id)
-    if (getDataCategory.length == 0) {
-      result = { category: 'Category not found.' }
+exports.validationDataProducts = async(data) => {
+    let result = null
+    if (data.name == null || data.name == '') {
+        result = { name: 'Name must be filled' }
     }
-  }
 
-  if (data.delivery_method_id == null || data.delivery_method_id == '') {
-    result = { ...result, category_id: 'id delivery method must be filled.' }
-  } else {
-    const getDeliveryMethod = await deliveryMethodModel.getDataDeliveryMethod(data.delivery_method_id)
-    if (getDeliveryMethod == 0) {
-      result = { delivery_method_id: 'id delivery method not found.' }
+    if (data.category_id == null || data.category_id == '') {
+        result = {...result, category_id: 'Id category must be filled.' }
+    } else {
+        const getDataCategory = await categoryModel.getDataCategory(data.category_id)
+        if (getDataCategory.length == 0) {
+            result = { category: 'Category not found.' }
+        }
     }
-  }
 
-  if (data.size_id == null || data.size_id == '') {
-    result = { ...result, size_id: 'id size must be filled.' }
-  } else {
-    const getDataSize = await sizeModel.getDataSize(data.size_id)
-    if (getDataSize == 0) {
-      result = { size: 'id size not found.' }
+    if (data.delivery_method_id == null || data.delivery_method_id == '') {
+        result = {...result, category_id: 'id delivery method must be filled.' }
+    } else {
+        const getDeliveryMethod = await deliveryMethodModel.getDataDeliveryMethod(data.delivery_method_id)
+        if (getDeliveryMethod == 0) {
+            result = { delivery_method_id: 'id delivery method not found.' }
+        }
     }
-  }
 
-  if (data.description == null || data.description == '') {
-    result = { ...result, description: 'Description must be filled.' }
-  }
+    if (data.size_id == null || data.size_id == '') {
+        result = {...result, size_id: 'id size must be filled.' }
+    } else {
+        const getDataSize = await sizeModel.getDataSize(data.size_id)
+        if (getDataSize == 0) {
+            result = { size: 'id size not found.' }
+        }
+    }
 
-  if (data.price == null || data.price == '') {
-    result = { ...result, price: 'Price must be filled' }
-  } else if (isNaN(parseInt(data.price))) {
-    result = { ...result, price: 'Price must be a number.' }
-  } else if (parseInt(data.price) == 0) {
-    result = { ...result, price: 'Price must be must be greater than 0.' }
-  }
+    if (data.description == null || data.description == '') {
+        result = {...result, description: 'Description must be filled.' }
+    }
 
-  if (data.stocks == null || data.stocks == '') {
-    result = { ...result, stocks: 'Stock must be filled' }
-  } else if (isNaN(parseInt(data.stocks))) {
-    result = { ...result, stocks: 'Stock must be a number.' }
-  } else if (parseInt(data.stocks) == 0) {
-    result = { ...result, stocks: 'Stock must be must be greater than 0.' }
-  }
+    if (data.price == null || data.price == '') {
+        result = {...result, price: 'Price must be filled' }
+    } else if (isNaN(parseInt(data.price))) {
+        result = {...result, price: 'Price must be a number.' }
+    } else if (parseInt(data.price) == 0) {
+        result = {...result, price: 'Price must be must be greater than 0.' }
+    }
 
-  if (data.delivery_time_start == null || data.delivery_time_start == '') {
-    result = { ...result, delivery_time_start: 'Delivery time start must be filled' }
-  }
+    if (data.stocks == null || data.stocks == '') {
+        result = {...result, stocks: 'Stock must be filled' }
+    } else if (isNaN(parseInt(data.stocks))) {
+        result = {...result, stocks: 'Stock must be a number.' }
+    } else if (parseInt(data.stocks) == 0) {
+        result = {...result, stocks: 'Stock must be must be greater than 0.' }
+    }
 
-  if (data.delivery_time_end == null || data.delivery_time_end == '') {
-    result = { ...result, delivery_time_end: 'Delivery time end must be filled' }
-  }
+    if (data.delivery_time_start == null || data.delivery_time_start == '') {
+        result = {...result, delivery_time_start: 'Delivery time start must be filled' }
+    }
 
-  return result
+    if (data.delivery_time_end == null || data.delivery_time_end == '') {
+        result = {...result, delivery_time_end: 'Delivery time end must be filled' }
+    }
+
+    return result
 }
 
 exports.validationPagination = (pagination) => {
-  let result = null
-  const { page, limit } = pagination
+    let result = null
+    const { page, limit } = pagination
 
-  if (isNaN(parseInt(page))) {
-    result = { ...result, page: 'Page must be a number.' }
-  } else if (page == 0) {
-    result = { ...result, page: 'Page must be grather then 0.' }
-  }
-
-  if (isNaN(parseInt(limit))) {
-    result = { ...result, limit: 'Limit must be a number.' }
-  } else if (limit == 0) {
-    result = { ...result, limit: 'Limit must be grather than 0.' }
-  }
-  return result
-}
-
-exports.validationLogin = async (data) => {
-  let result = null
-
-  if (!data.email || data.email === '') {
-    result = { email: 'Email must be filled.' }
-  }
-  if (!data.password || data.password === '') {
-    result = { ...result, password: 'Password must be filled.' }
-  }
-  return result
-}
-
-exports.validationRegister = async (data) => {
-  let result = null
-
-  if (!data.email || data.email == '') {
-    result = { email: 'Email must be filled.' }
-  } else {
-    const resultEmail = await userModel.getDataUerByEmail(data.email)
-    console.log(resultEmail)
-    if (resultEmail.length > 0) {
-      result = { email: 'Email has already used.' }
+    if (isNaN(parseInt(page))) {
+        result = {...result, page: 'Page must be a number.' }
+    } else if (page == 0) {
+        result = {...result, page: 'Page must be grather then 0.' }
     }
-  }
 
-  if (!data.phone || data.phone == '') {
-    result = { ...result, fullName: 'phone must be filled.' }
-  }
-
-  if (!data.password || data.password == '') {
-    result = { ...result, password: 'Password must be filled.' }
-  }
-  return result
+    if (isNaN(parseInt(limit))) {
+        result = {...result, limit: 'Limit must be a number.' }
+    } else if (limit == 0) {
+        result = {...result, limit: 'Limit must be grather than 0.' }
+    }
+    return result
 }
+
+exports.validationLogin = async(data) => {
+    let result = null
+
+    if (!data.email || data.email === '') {
+        result = { email: 'Email must be filled.' }
+    }
+    if (!data.password || data.password === '') {
+        result = {...result, password: 'Password must be filled.' }
+    }
+    return result
+}
+
+exports.validationRegister = async(data) => {
+    let result = null
+
+    if (!data.email || data.email == '') {
+        result = { email: 'Email must be filled.' }
+    } else {
+        const resultEmail = await userModel.getDataUerByEmail(data.email)
+        console.log(resultEmail)
+        if (resultEmail.length > 0) {
+            result = { email: 'Email has already used.' }
+        }
+    }
+
+    if (!data.phone || data.phone === '') {
+        result = {...result, phone: 'phone must be filled.' }
+    }
+
+    if (!data.password || data.password === '') {
+        result = {...result, password: 'Password must be filled.' }
+    }
+    return result
+}
+
+exports.validationUser = async(data) => {
+    let result = null
+
+    if (!data.email || data.email == '') {
+        result = { email: 'Email must be filled.' }
+    } else {
+        const resultEmail = await userModel.getDataUerByEmailUpdate(data.email, data.id)
+        console.log(resultEmail)
+        if (resultEmail.length > 0) {
+            result = { email: 'Email has already used.' }
+        }
+    }
+
+    if (!data.phone || data.phone === '') {
+        result = {...result, phone: 'phone must be filled.' }
+    }
+
+    if (!data.password || data.password === '') {
+        result = {...result, password: 'Password must be filled.' }
+    }
+
+    if (!data.display_name || data.display_name === '') {
+        result = {...result, display_name: 'Display name must be filled.' }
+    }
+
+    if (!data.first_name || data.first_name === '') {
+        result = {...result, first_name: 'Firstname must be filled.' }
+    }
+
+    if (!data.last_name || data.last_name === '') {
+        result = {...result, last_name: 'Lastname must be filled.' }
+    }
+    if (!data.gender || data.gender === '') {
+        result = {...result, gender: 'Gender must be filled.' }
+    }
+    if (!data.birthdate || data.birthdate === '') {
+        result = {...result, birthdate: 'Birthdate must be filled.' }
+    }
+    return result
+}
+
 
 // exports.validateProduct = [
 //     body('name')
