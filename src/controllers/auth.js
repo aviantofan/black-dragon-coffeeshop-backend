daea361dbe7ed51cf91bf0f3e2f5477e9c9dfaab
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-11 21:43:41 +0700

    feat : update hash password using bcrypt

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,81 +1,82 @@
 /* eslint-disable no-unused-vars */
 const userModel = require('../models/user')
-const argon = require('argon2')
+const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const showApi = require('../helpers/showResponse')
 const validation = require('../helpers/validation')
 const { APP_SECRET } = process.env
 
-const login = async (req, res) => {
-  const dataLogin = { email: req.body.email, password: req.body.password }
-  const errValidation = await validation.validationLogin(dataLogin)
-  if (errValidation == null) {
-    const dataUser = await userModel.getDataUerByEmail(dataLogin.email)
-    if (dataUser.length > 0) {
-      const { password: hashPassword } = dataUser[0]
-      const checkPassword = await argon.verify(hashPassword, dataLogin.password)
-      if (checkPassword) {
-        if (dataUser[0].confirm == 1) {
-          const data = { id: dataUser[0].id }
-          const token = jwt.sign(data.id, APP_SECRET)
-          return showApi.showResponse(res, 'Login Success!', { token })
+const login = async(req, res) => {
+    const dataLogin = { email: req.body.email, password: req.body.password }
+    const errValidation = await validation.validationLogin(dataLogin)
+    if (errValidation == null) {
+        const dataUser = await userModel.getDataUerByEmail(dataLogin.email)
+        if (dataUser.length > 0) {
+            const { password: hashPassword } = dataUser[0]
+            const checkPassword = await bcrypt.compare(dataLogin.password, hashPassword)
+            if (checkPassword) {
+                if (dataUser[0].confirm == 1) {
+                    const data = { id: dataUser[0].id }
+                    const token = jwt.sign(data.id, APP_SECRET)
+                    return showApi.showResponse(res, 'Login Success!', { token })
+                } else {
+                    return showApi.showResponse(res, 'User not authorized', null, null, 404)
+                }
+            } else {
+                return showApi.showResponse(res, 'Wrong email and password!', null, null, 400)
+            }
         } else {
-          return showApi.showResponse(res, 'User not authorized', null, null, 404)
+            return showApi.showResponse(res, 'Wrong email and password!', null, null, 400)
         }
-      } else {
-        return showApi.showResponse(res, 'Wrong email and password!', null, null, 400)
-      }
     } else {
-      return showApi.showResponse(res, 'Wrong email and password!', null, null, 400)
+        return showApi.showResponse(res, 'Data login not valid!', null, errValidation, 400)
     }
-  } else {
-    return showApi.showResponse(res, 'Data login not valid!', null, errValidation, 400)
-  }
 }
 
-const register = async (req, res) => {
-  const { email, password, phone } = req.body
-  const data = { email, password, phone }
-  const errValidation = await validation.validationRegister(data)
-  if (errValidation == null) {
-    const hashPassword = await argon.hash(data.password)
-    data.password = hashPassword
-    try {
-      const resultDataUser = await userModel.insertDataUser(data)
-      if (resultDataUser.affectedRows > 0) {
-        const resultDataRegister = await userModel.insertDataUserProfile(data)
-        if (resultDataRegister.affectedRows > 0) {
-          return showApi.showResponse(res, 'Registration Success!')
-        }
+const register = async(req, res) => {
+    const { email, password, phone } = req.body
+    const data = { email, password, phone }
+    const errValidation = await validation.validationRegister(data)
+    if (errValidation == null) {
+        const salt = await bcrypt.genSalt(8)
+        const hashPassword = await bcrypt.hash(data.password, salt)
+        data.password = hashPassword
+        try {
+            const resultDataUser = await userModel.insertDataUser(data)
+            if (resultDataUser.affectedRows > 0) {
+                const resultDataRegister = await userModel.insertDataUserProfile(data)
+                if (resultDataRegister.affectedRows > 0) {
+                    return showApi.showResponse(res, 'Registration Success!')
+                }
 
-        // let randomCode = Math.round(Math.random() * (9999999 - 100000) - 100000);
-        // if (randomCode < 0) {
-        //     randomCode = (randomCode * -1);
-        // }
-        // const reset = await emailVerificationModel.insertEmailVerification(resultRegister.insertId, randomCode);
-        // if (reset.affectedRows >= 1) {
-        //     await mail.sendMail({
-        //         from: APP_EMAIL,
-        //         to: email,
-        //         subject: 'Email Verification',
-        //         text: String(randomCode),
-        //         html: `This is your email verification code : <b>${randomCode}</b>`
-        //     });
-        //     dataJson = {...dataJson, message: "Email Verification has been sent to your email!" };
-        //     return showApi.showSuccess(dataJson);
-        // } else {
-        //     dataJson = {...dataJson, message: "Email Verification failed to send." };
-        //     return showApi.showSuccess(dataJson);
-        // }
-      } else {
-        return showApi.showResponse(res, 'Registration failed!', null, null, 500)
-      }
-    } catch (error) {
-      return showApi.showResponse(res, 'Registration failed!', null, error.message, 500)
+                // let randomCode = Math.round(Math.random() * (9999999 - 100000) - 100000);
+                // if (randomCode < 0) {
+                //     randomCode = (randomCode * -1);
+                // }
+                // const reset = await emailVerificationModel.insertEmailVerification(resultRegister.insertId, randomCode);
+                // if (reset.affectedRows >= 1) {
+                //     await mail.sendMail({
+                //         from: APP_EMAIL,
+                //         to: email,
+                //         subject: 'Email Verification',
+                //         text: String(randomCode),
+                //         html: `This is your email verification code : <b>${randomCode}</b>`
+                //     });
+                //     dataJson = {...dataJson, message: "Email Verification has been sent to your email!" };
+                //     return showApi.showSuccess(dataJson);
+                // } else {
+                //     dataJson = {...dataJson, message: "Email Verification failed to send." };
+                //     return showApi.showSuccess(dataJson);
+                // }
+            } else {
+                return showApi.showResponse(res, 'Registration failed!', null, null, 500)
+            }
+        } catch (error) {
+            return showApi.showResponse(res, 'Registration failed!', null, error.message, 500)
+        }
+    } else {
+        return showApi.showResponse(res, 'Data not valid.', errValidation, 400)
     }
-  } else {
-    return showApi.showResponse(res, 'Data not valid.', errValidation, 400)
-  }
 }
 
 // const emailVerification = async(req, res) => {
