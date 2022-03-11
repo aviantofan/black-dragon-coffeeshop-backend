3e6fea329ddc6fc187c8ccbd6732c17209518323
eexxprd <aviantofan@gmail.com>
2022-03-11 13:46:41 +0700

    chore: add model category

--- src/models/category.js
+++ src/models/category.js
@@ -1,7 +1,14 @@
 const db = require('../helpers/database')
 
+exports.getDataCategories = () => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM categories', (err, res) => {
+    if (err) reject(err)
+    resolve(res)
+  })
+})
+
 exports.getDataCategory = (id) => new Promise((resolve, reject) => {
-  db.query('select * from categories where id=?', [id], (err, res) => {
+  db.query('SELECT * FROM categories where id=?', [id], (err, res) => {
     if (err) reject(err)
     resolve(res)
   })
