4181976da8e2f493d5fca93082acb9c7db70fe29
Nabil Hardiansa <nhardiansa@gmail.com>
2022-03-11 15:13:57 +0800

    feat: merged result

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-const express = require('express');
-const route = new express.Router();
-
-module.exports = route;
-=======
 const route = require('express').Router()
 
 route.use('/products', require('./product'))
 
-module.exports = route
->>>>>>> 153fdd5d145d2424bffb8b3ef55ba01d06959187
+module.exports = route;
