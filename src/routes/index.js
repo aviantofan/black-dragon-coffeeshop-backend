6497559ee2936a800a45658a75430b5c4dd462c5
eexxprd <aviantofan@gmail.com>
2022-03-11 19:05:46 +0700

    feat: add category routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 const express = require('express');
 const route = new express.Router();
 
 module.exports = route;
-=======
-const route = require('express').Router()
 
-route.use('/products', require('./product'))
+route.use('/products', require('./product'));
+route.use('/categories', require('./category'));
 
-module.exports = route
->>>>>>> 153fdd5d145d2424bffb8b3ef55ba01d06959187
+module.exports = route;
