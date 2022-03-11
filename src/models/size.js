e1be607b4189dfb9c0ec557820d50f73c24f257c
eexxprd <aviantofan@gmail.com>
2022-03-11 13:54:02 +0700

    chore: add model size

--- src/models/size.js
+++ src/models/size.js
@@ -1,8 +1,15 @@
-const db = require('../helpers/database')
+const db = require('../helpers/database');
+
+exports.getDataSizes = () => new Promise((resolve, reject) => {
+  db.query('SELECT * FROM sizes', (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
 
 exports.getDataSize = (id) => new Promise((resolve, reject) => {
-  db.query('select * from sizes where id=?', [id], (err, res) => {
-    if (err) reject(err)
-    resolve(res)
-  })
-})
+  db.query('SELECT * FROM sizes WHERE id=?', [id], (err, res) => {
+    if (err) reject(err);
+    resolve(res);
+  });
+});
