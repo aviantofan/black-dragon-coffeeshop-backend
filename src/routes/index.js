36d962b6ee77fa5b411c25bf99550c9cdedda145
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-12 18:32:01 +0700

    feat : add route product delivery method

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,5 +4,6 @@ route.use('/products', require('./product'))
 route.use('/auth', require('./auth'))
 route.use('/histories', require('./histories'))
 route.use('/sizeForProduct', require('./sizeForProduct'))
+route.use('/productDeliveryMethod', require('./productDeliveryMethod'))
 
 module.exports = route
