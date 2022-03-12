6598ea88345ba5fbcd42fb45023bdccf0deb1c14
eexxprd <aviantofan@gmail.com>
2022-03-13 02:05:13 +0700

    feat: fix validation for products

--- src/helpers/validation.js
+++ src/helpers/validation.js
@@ -1,69 +1,38 @@
-const {
-  body,
-  validationResult
-} = require('express-validator');
+const { body, validationResult } = require('express-validator');
 const categoryModel = require('../models/category');
 const deliveryMethodModel = require('../models/deliveryMethod');
 const sizeModel = require('../models/size');
 
 exports.validationDataProducts = async (data) => {
   let result = null;
-  if (data.name == null || data.name == '') {
+  if (data.name == null || data.name === '') {
     result = {
       name: 'Name must be filled'
     };
   }
 
-  if (data.category_id == null || data.category_id == '') {
+  if (data.category_id == null || data.category_id === '') {
     result = {
       ...result,
       category_id: 'Id category must be filled.'
     };
   } else {
     const getDataCategory = await categoryModel.getDataCategory(data.category_id);
-    if (getDataCategory.length == 0) {
+    if (getDataCategory.length === 0) {
       result = {
         category: 'Category not found.'
       };
     }
   }
 
-  if (data.delivery_method_id == null || data.delivery_method_id == '') {
-    result = {
-      ...result,
-      category_id: 'id delivery method must be filled.'
-    };
-  } else {
-    const getDeliveryMethod = await deliveryMethodModel.getDataDeliveryMethod(data.delivery_method_id);
-    if (getDeliveryMethod == 0) {
-      result = {
-        delivery_method_id: 'id delivery method not found.'
-      };
-    }
-  }
-
-  if (data.size_id == null || data.size_id == '') {
-    result = {
-      ...result,
-      size_id: 'id size must be filled.'
-    };
-  } else {
-    const getDataSize = await sizeModel.getDataSize(data.size_id);
-    if (getDataSize == 0) {
-      result = {
-        size: 'id size not found.'
-      };
-    }
-  }
-
-  if (data.description == null || data.description == '') {
+  if (data.description == null || data.description === '') {
     result = {
       ...result,
       description: 'Description must be filled.'
     };
   }
 
-  if (data.price == null || data.price == '') {
+  if (data.price == null || data.price === '') {
     result = {
       ...result,
       price: 'Price must be filled'
@@ -73,14 +42,14 @@ exports.validationDataProducts = async (data) => {
       ...result,
       price: 'Price must be a number.'
     };
-  } else if (parseInt(data.price) == 0) {
+  } else if (parseInt(data.price) === 0) {
     result = {
       ...result,
       price: 'Price must be must be greater than 0.'
     };
   }
 
-  if (data.stocks == null || data.stocks == '') {
+  if (data.stocks == null || data.stocks === '') {
     result = {
       ...result,
       stocks: 'Stock must be filled'
@@ -90,21 +59,21 @@ exports.validationDataProducts = async (data) => {
       ...result,
       stocks: 'Stock must be a number.'
     };
-  } else if (parseInt(data.stocks) == 0) {
+  } else if (parseInt(data.stocks) === 0) {
     result = {
       ...result,
       stocks: 'Stock must be must be greater than 0.'
     };
   }
 
-  if (data.delivery_time_start == null || data.delivery_time_start == '') {
+  if (data.delivery_time_start == null || data.delivery_time_start === '') {
     result = {
       ...result,
       delivery_time_start: 'Delivery time start must be filled'
     };
   }
 
-  if (data.delivery_time_end == null || data.delivery_time_end == '') {
+  if (data.delivery_time_end == null || data.delivery_time_end === '') {
     result = {
       ...result,
       delivery_time_end: 'Delivery time end must be filled'
@@ -116,7 +85,7 @@ exports.validationDataProducts = async (data) => {
 
 exports.validationDataCategory = (data) => {
   let result = null;
-  if (data.name == null || data.name == '') {
+  if (data.name == null || data.name === '') {
     result = {
       name: 'Name must be filled'
     };
@@ -136,7 +105,7 @@ exports.validationPagination = (pagination) => {
       ...result,
       page: 'Page must be a number.'
     };
-  } else if (page == 0) {
+  } else if (page === 0) {
     result = {
       ...result,
       page: 'Page must be grather then 0.'
@@ -148,7 +117,7 @@ exports.validationPagination = (pagination) => {
       ...result,
       limit: 'Limit must be a number.'
     };
-  } else if (limit == 0) {
+  } else if (limit === 0) {
     result = {
       ...result,
       limit: 'Limit must be grather than 0.'
