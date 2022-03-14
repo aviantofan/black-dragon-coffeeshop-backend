36f26c1af8b4da51ef8145c1c30c4d15afee12c2
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-14 20:47:37 +0700

    fix : fix module  promotion delivery  method

--- src/routes/promotionDeliveryMethod.js
+++ src/routes/promotionDeliveryMethod.js
@@ -1,18 +1,24 @@
-
 const promotionDeliveryMethods = require('express').Router();
 
 const {
   insertPromotionDeliveryMethod,
   getPromotionDeliveryMethods,
+  getListPromotionByIdPromotion,
   getPromotionDeliveryMethod,
-  updatePatchPromotionDeliveryMethod,
-  deletePromotionDeliveryMethod
+  updatePatchPromotionDeliveryMethod2,
+  // updatePatchPromotionDeliveryMethod,
+  // deletePromotionDeliveryMethod
+  deletePromotionDeliveryMethod2
 } = require('../controllers/promotionDeliveryMethod');
 
 promotionDeliveryMethods.post('/', insertPromotionDeliveryMethod);
 promotionDeliveryMethods.get('/', getPromotionDeliveryMethods);
 promotionDeliveryMethods.get('/:id', getPromotionDeliveryMethod);
-promotionDeliveryMethods.patch('/:id', updatePatchPromotionDeliveryMethod);
-promotionDeliveryMethods.delete('/:id', deletePromotionDeliveryMethod);
+promotionDeliveryMethods.get('/promo/:idPromotion', getListPromotionByIdPromotion);
+
+// promotionDeliveryMethods.patch('/:id', updatePatchPromotionDeliveryMethod);
+promotionDeliveryMethods.patch('/:id', updatePatchPromotionDeliveryMethod2);
+// promotionDeliveryMethods.delete('/:id', deletePromotionDeliveryMethod);
+promotionDeliveryMethods.delete('/:id', deletePromotionDeliveryMethod2);
 
 module.exports = promotionDeliveryMethods;
