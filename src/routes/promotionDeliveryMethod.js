a790f99d5b9f3de6a93dbbd0dbf13306dfd5ffe2
marsellavaleria19 <marsellavaleria19@gmail.com>
2022-03-15 07:21:22 +0700

    feat : init

--- src/routes/promotionDeliveryMethod.js
+++ src/routes/promotionDeliveryMethod.js
@@ -1,21 +1,23 @@
 const promotionDeliveryMethods = require('express').Router();
 
 const {
-    // insertPromotionDeliveryMethod,
-    insertPromotionDeliveryMethod2,
-    getPromotionDeliveryMethods,
-    getListPromotionDeliveryMethodByIdPromotion,
-    getPromotionDeliveryMethod,
-    updatePatchPromotionDeliveryMethod2,
-    // updatePatchPromotionDeliveryMethod,
-    // deletePromotionDeliveryMethod
-    deletePromotionDeliveryMethod2
+  // insertPromotionDeliveryMethod,
+  insertPromotionDeliveryMethod2,
+  getPromotionDeliveryMethods,
+  getListPromotionDeliveryMethodByIdPromotion,
+  // getPromotionDeliveryMethod,
+  getPromotionDeliveryMethod2,
+  updatePatchPromotionDeliveryMethod2,
+  // updatePatchPromotionDeliveryMethod,
+  // deletePromotionDeliveryMethod
+  deletePromotionDeliveryMethod2
 } = require('../controllers/promotionDeliveryMethod');
 
 // promotionDeliveryMethods.post('/', insertPromotionDeliveryMethod);
-promotionDeliveryMethods.post('/', insertPromotionDeliveryMethod2)
+promotionDeliveryMethods.post('/', insertPromotionDeliveryMethod2);
 promotionDeliveryMethods.get('/', getPromotionDeliveryMethods);
-promotionDeliveryMethods.get('/:id', getPromotionDeliveryMethod);
+// promotionDeliveryMethods.get('/:id', getPromotionDeliveryMethod);
+promotionDeliveryMethods.get('/:id', getPromotionDeliveryMethod2);
 promotionDeliveryMethods.get('/promo/:idPromotion', getListPromotionDeliveryMethodByIdPromotion);
 // promotionDeliveryMethods.patch('/:id', updatePatchPromotionDeliveryMethod);
 promotionDeliveryMethods.patch('/:id', updatePatchPromotionDeliveryMethod2);
